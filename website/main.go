01f83524c40dfc1070e1a6feedf365042df6e2e9
Peter Zam <mail@peterzam.dev>
2022-09-21 15:39:26 +0630

    fix env path

--- website/main.go
+++ website/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"Xpress/routes"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	e := godotenv.Load()
+	e := godotenv.Load("../.env")
 	r := routes.Handlers()
 
 	if e != nil {
 		log.Fatal("Error Loading .env file")
 	}
-	// port := os.Getenv("PORT")
-	r.Run(":" + "8080")
+	port := os.Getenv("SERVER_PORT")
+	r.Run(":" + port)
 }
