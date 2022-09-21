dd8a963f18f19d1395b32b8aeac348b606884967
Peter Zam <mail@peterzam.dev>
2022-09-21 22:25:57 +0630

    fix types & names

--- init-db.sql
+++ init-db.sql
@@ -1,13 +1,13 @@
 USE 'xpress_db';
 
-CREATE TABLE User(
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(30) NOT NULL UNIQUE,
-    user_password VARCHAR(32) NOT NULL,
-    user_role VARCHAR(10) NOT NULL,
-    user_active BOOLEAN
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(32) NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    active BOOLEAN
 );
 
-INSERT INTO User (user_name, user_password, user_role, user_active) VALUES
+INSERT INTO users (name, password, role, active) VALUES
 ('admin','21232f297a57a5a743894a0e4a801fc3','admin',true),
 ('user','ee11cbb19052e40b07aac0ca060c23ee','user',false);
