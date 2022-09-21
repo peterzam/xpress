77d8c64a4ca93022b0ca041b975d04eb3762aa60
Peter Zam <mail@peterzam.dev>
2022-09-21 17:34:50 +0630

    fix active

--- website/models/token.go
+++ website/models/token.go
@@ -4,8 +4,9 @@ import jwt "github.com/golang-jwt/jwt"
 
 // Token struct declaration
 type Token struct {
-	Id   uint
-	Name string
-	Role string
+	Id     uint
+	Name   string
+	Role   string
+	Active bool
 	*jwt.StandardClaims
 }
